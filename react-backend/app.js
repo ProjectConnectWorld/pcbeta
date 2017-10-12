009d19c65e34701a22d75d952f99f14b1610cfaa
ayanez17 <fredyyanez39@yahoo.com>
2017-10-12 17:10:52 -0400

    generic

--- react-backend/app.js
+++ react-backend/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors')
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -32,7 +33,7 @@ app.use(function(req, res, next) {
 });
 
 
-
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false

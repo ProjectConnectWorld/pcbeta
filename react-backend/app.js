8ed7544b750cb94027f44ff5eef161942f076a68
ayanez17 <fredyyanez39@yahoo.com>
2017-10-12 16:55:48 -0400

    generic

--- react-backend/app.js
+++ react-backend/app.js
@@ -25,9 +25,9 @@ console.log(bodyParser.json());
 // });
 
 app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT ,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT ,DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
   next();
 });
 

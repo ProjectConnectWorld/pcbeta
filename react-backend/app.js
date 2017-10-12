6f204139316b45c53c2f7841a21c59b48dbf66cc
ayanez17 <fredyyanez39@yahoo.com>
2017-10-12 19:25:21 -0400

    generic

--- react-backend/app.js
+++ react-backend/app.js
@@ -23,23 +23,23 @@ app.set('view engine', 'jade');
 //app.use(logger('dev'));
 console.log(bodyParser.json());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
-});
-
-// app.all('*', function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', 'URLs to trust of allow');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type');
-//   if ('OPTIONS' == req.method) {
-//     res.sendStatus(200);
-//   } else {
-//     next();
-//   }
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//   next();
 // });
+
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'URLs to trust of allow');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if ('OPTIONS' == req.method) {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 
 //app.use(cors())

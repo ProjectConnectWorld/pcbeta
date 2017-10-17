59dd91bdcaef9a8496090e27239bbf4e27010f4d
ayanez17 <fredyyanez39@yahoo.com>
2017-10-17 15:50:21 -0400

    generic

--- src/components/leftPanel.jsx
+++ src/components/leftPanel.jsx
@@ -20,7 +20,7 @@ class LeftPanel extends Component {
     return (
       <div className="left-mod">
         <div className="label">
-          {this.props.countrySelected.countryname} Attributes
+          {this.props.countrySelected.countryname} {this.props.countrySelected.admin1} Attributes
         </div>
         <div className="dynamic">
 

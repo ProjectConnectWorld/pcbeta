8b1dcd52eee18774f5d88daf62588b48f9ae03d1
ayanez17 <fredyyanez39@yahoo.com>
2017-10-24 15:00:39 -0400

    generic

--- src/components/simpleExample.jsx
+++ src/components/simpleExample.jsx
@@ -323,7 +323,7 @@ class SimpleExample extends Component {
       color: this.getStyle(feature.properties.speed_connectivity, feature.properties.type_connectivity),
       fillColor: this.getStyle(feature.properties.speed_connectivity),
       fillOpacity: .6,
-      radius: 1
+      radius: 2
     }).bindPopup(popUpString(feature.properties)); // Change marker to circle
   }
 

65585ca40f78f8c6f594c71c43514d78292afe79
ayanez17 <fredyyanez39@yahoo.com>
2017-10-25 15:47:07 -0400

    generic

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -24,10 +24,10 @@ class Naver extends Component {
     return (
       <Navbar>
         <Navbar.Brand pullLeft>
-          <a href="http://projectconnect.world"><img src={logo} height= "130%" alt=""/></a>
+          <a href="http://projectconnect.world"><img src={logo} height= "200%" alt=""/></a>
         </Navbar.Brand>
         <Navbar.Brand pullRight className="nav2">
-          <a href="#"><img src={logo2} height= "130%" alt=""/></a>
+          <a href="#"><img src={logo2} height= "200%" alt=""/></a>
         </Navbar.Brand>
 
 
